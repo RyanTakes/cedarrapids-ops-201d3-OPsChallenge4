10f35cbf079bd2c01c5b381ad28f3f2c5e7aabc3
Ryan Takes <77939328+RyanTakes@users.noreply.github.com>
2021-02-04 14:45:47 -0600

    Add files via upload

---
+++ array.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+dir1="dir1"
+dir2="dir2"
+dir3="dir3"
+dir4="dir4"
+
+arraydir=("/home/ryan/dir1" "/home/ryan/dir2" "/home/ryan/dir3" "/home/ryan/dir4")
+
+echo "${arraydir[]}" > array.txt
